7c4b956c1bd2c28ee84ac97788e2274b91ffeae5
Francis <francismuasya@gmail.com>
2020-10-05 15:15:16 +0300

    added schema

--- schema.sql
+++ schema.sql
@@ -3,17 +3,15 @@ create table customers (
     first_name varchar(100),
     last_name varchar(100),
     email varchar(300)
-)
-
+);
 create table items(
     id serial primary key,
     name varchar(200),
     sku varchar(50),
     inventory integer
-)
-
+);
 create table purchases (
     id serial primary key,
     item_id integer references items(id),
     purchase_qty integer
-)
+);
---
+++ seed.sql
@@ -0,0 +1,12 @@
+INSERT into customers(first_name, last_name, email) 
+VALUES ('John', 'Smith', 'jsmith@email.com'),
+('Bob', 'Robertson', 'bobistheman@bob.com');
+-- create some items that customers can purchase
+INSERT into items(name, sku, inventory)
+VALUES ('antenna', 'A100', 50),
+('mast', 'A101', 5),
+('bracket', 'A102', 10),
+('Switch', 'A103', 19);
+-- create a purchase
+INSERT into purchases(item_id, purchase_qty)
+VALUES (4, 1);
